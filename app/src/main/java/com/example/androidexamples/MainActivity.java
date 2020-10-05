fcad4b6e45dee0382ca897e89a2f1fb4c2c3ab62
William Peltier <pelt0029@algonquinlive.com>
2020-10-05 15:03:29 -0400

    Lab3 step 5

--- app/src/main/java/com/example/androidexamples/MainActivity.java
+++ app/src/main/java/com/example/androidexamples/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.androidexamples;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,27 +20,51 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_lab3_linear);
 
+        SharedPreferences prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
 
+        String email = prefs.getString("email", "");
+        EditText typeField = findViewById(R.id.email);
+        typeField.setText(email);
 
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast toast = Toast.makeText(getApplicationContext(),
                         getResources().getString(R.string.toast_message), Toast.LENGTH_LONG);
-                toast.show();
-            }
+                toast.show(); }
         });
 
         Switch switcher = findViewById(R.id.switcher);
         TextView myTextView = findViewById(R.id.switcher);
-
         switcher.setOnCheckedChangeListener( (whatClicked, newState) -> {
             Snackbar.make(myTextView,getResources().getString(R.string.switch_message) + newState, Snackbar.LENGTH_SHORT).setAction( "Undo", click -> switcher.setChecked(!newState)).show();
-
         });
 
+        final Button login = findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                 }
+        });
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        EditText typeField = findViewById(R.id.email);
+        saveSharedPrefs(typeField.getText().toString());
+    }
+
+    private void saveSharedPrefs(String stringToSave){
+        SharedPreferences prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("email", stringToSave);
+        editor.commit();
     }
 
 }
